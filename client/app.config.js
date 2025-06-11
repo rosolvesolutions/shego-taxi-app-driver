53519cdd9e9c975997d2e2dc6c95bb14ec6dbdf3
janssenPeng <148623351+janssenPeng@users.noreply.github.com>
2025-06-11 17:09:45 +0100

    fixed map api

--- client/app.config.js
+++ client/app.config.js
@@ -1,8 +1,7 @@
-import 'dotenv/config';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
 
-const googleKeyPath = path.resolve(__dirname, '../server/keys/google-maps-key.json');
+const googleKeyPath = path.resolve(__dirname, './keys/google-maps-key.json');
 const googleKeyJson = JSON.parse(fs.readFileSync(googleKeyPath, 'utf-8'));
 const GOOGLE_MAPS_API_KEY = googleKeyJson.apiKey;
 
@@ -17,33 +16,29 @@ export default {
     userInterfaceStyle: 'automatic',
     newArchEnabled: true,
     platforms: ['ios', 'android'],
-
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.rosolve.taxi',
       config: {
         googleMapsApiKey: GOOGLE_MAPS_API_KEY,
       },
+      bundleIdentifier: 'com.rosolve.taxi',
     },
-
     android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/images/adaptive-icon.png',
-        backgroundColor: '#ffffff',
-      },
       config: {
         googleMaps: {
           apiKey: GOOGLE_MAPS_API_KEY,
         },
       },
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
     },
-
     web: {
       bundler: 'metro',
       output: 'static',
       favicon: './assets/images/favicon.png',
     },
-
     plugins: [
       'expo-router',
       [
@@ -56,13 +51,11 @@ export default {
         },
       ],
     ],
-
     experiments: {
       typedRoutes: true,
     },
-
     extra: {
-      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.107:5001',
     },
   },
 };
